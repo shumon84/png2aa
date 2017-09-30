f060762b9d613d64446bf7a59da06dca106eb7b1
shumon84 <cameremon84@gmail.com>
2017-09-30 16:48:40 +0900

    Modified Edit according to color options

--- bound_box.c
+++ bound_box.c
@@ -1,5 +1,13 @@
 #include"image.h"
-extern int opt_bound_box;
+extern int opt_color;
+
+int is_black(pixcel_t p)
+{
+  if(opt_color)
+    return p.c.r!=0x00 && p.c.g!=0x00 && p.c.b!=0x00; 
+  else
+    return p.g!=0x00;
+}
 
 image_t* make_bound_box(image_t *img)
 {  
@@ -13,7 +21,7 @@ image_t* make_bound_box(image_t *img)
 
   for(i=0;i<y;i++)
     for(j=0;j<img->width;j++)
-      if(img->map[i][j].g!=0x00)
+      if(is_black(img->map[i][j]))
 	{
 	  miny=i;
 	  i=y;
@@ -21,7 +29,7 @@ image_t* make_bound_box(image_t *img)
 	}
   for(i=img->height-1;i>y;i--)
     for(j=0;j<img->width;j++)
-      if(img->map[i][j].g!=0x00)
+      if(is_black(img->map[i][j]))
 	{
 	  maxy=i;
 	  i=y;
@@ -29,7 +37,7 @@ image_t* make_bound_box(image_t *img)
 	}
   for(i=0;i<x;i++)
     for(j=0;j<img->height;j++)
-      if(img->map[j][i].g!=0x00)
+      if(is_black(img->map[j][i]))
 	{
 	  minx=i;
 	  i=x;
@@ -37,20 +45,20 @@ image_t* make_bound_box(image_t *img)
 	}
   for(i=img->width-1;i>x;i--)
     for(j=0;j<img->height;j++)
-      if(img->map[j][i].g!=0x00)
+      if(is_black(img->map[j][i]))
 	{
 	  maxx=i;
 	  i=x;
 	  break;
 	}
-
   uint32_t width=maxx-minx+1;
   uint32_t height=maxy-miny+1;
-  image_t *bound_box=allocate_image(width,height,COLOR_TYPE_GRAY);
+
+  image_t *bound_box=allocate_image(width,height,opt_color?COLOR_TYPE_RGB:COLOR_TYPE_GRAY);
   
   for(i=0;i<height;i++)
     for(j=0;j<width;j++)
-      bound_box->map[i][j].g=img->map[miny+i][minx+j].g;
-  
+      bound_box->map[i][j]=img->map[miny+i][minx+j];
+
   return bound_box;
 }
