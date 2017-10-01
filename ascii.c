1dcc1cfdc878190c5a5f6635323dda01246b573c
shumon84 <cameremon84@gmail.com>
2017-10-02 08:15:59 +0900

    Fix bug that vertical image can not be convert

--- ascii.c
+++ ascii.c
@@ -110,15 +110,13 @@ double white_ratio(image_t *img,uint32_t y1,uint32_t y2,uint32_t x1,uint32_t x2)
   uint64_t sum=0;
   uint64_t n=0;
   
-  for(i=y1;i<y2&&i<img->height;i++)
-    for(j=x1;j<x2&&i<img->width;j++)
+  for(i=y1;i<y2;i++)
+    for(j=x1;j<x2;j++)
       {
 	sum+=img->map[i][j].g;
 	n++;
       }
-  printf("%llu/%llu,",sum,n); // debug
   double ratio=(double)sum/n;
-  //  printf("%.2f,",ratio); // debug
   if(opt_white)ratio=255-ratio;
   return ratio/255*0.268091;
 }
@@ -135,6 +133,5 @@ void block_to_ascii(image_t *img,double unitH,double unitW,uint32_t y,uint32_t x
     ascii=palette[i].ascii;
   else
     ascii=palette[i-1].ascii;
-  //  putchar(ascii);
+   putchar(ascii);
 }
-  
