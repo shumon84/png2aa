09b1e0605cd4759ae185d19477cde75a9cd96351
shumon84 <cameremon84@gmail.com>
2017-10-02 16:16:51 +0900

    Update homebrew formura

--- png2aa.rb
+++ png2aa.rb
@@ -1,9 +1,9 @@
 class Png2aa < Formula
   desc ""
   homepage "https://github.com/shumon84/png2aa"
-  url "https://github.com/shumon84/png2aa/archive/v1.1.tar.gz"
-  version "1.1"
-  sha256 "636c4f388dc541b6a15e201d62d45131ae85be931f8f29b56d65d0a5286f29c2"
+  url "https://github.com/shumon84/png2aa/archive/v1.2.tar.gz"
+  version "1.2"
+  sha256 "166e93d543165bcf32462564353e13d167b5d098451cb8641fed3a2e5ef324e9"
 
   depends_on "libpng"
   depends_on "jpeg"
